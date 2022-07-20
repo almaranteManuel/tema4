b063cf783e126decbd7dc02553c2cdc89d51702b
manuco <almarante.manu@gmail.com>
2022-07-20 02:15:47 -0300

    falta el do while

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+			double ifNumero = 700;
+			
+			if (ifNumero < 0) {
+				System.out.println("Este numero es negativo");
+			} else if (ifNumero > 0) {
+				System.out.println("Este numero es positivo");
+			} else {
+				System.out.println("este numero es 0");
+			}
+			
+			
+			int numeroWhile = 0;
+			while (numeroWhile < 3) {
+				System.out.println(numeroWhile);
+				numeroWhile = numeroWhile + 1;
+				
+			}
+				
+		}
+
+	}
+
+
